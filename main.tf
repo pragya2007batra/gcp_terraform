580ad3e19c94d7e7f0ea03652a79c7b9db568d1c
pragya2007batra <pragya2007batra@gmail.com>
2026-07-21 21:25:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,19 @@
 terraform {
- required_providers {
- google = {
- source = "hashicorp/google"
- version = "~> 6.0"
- }
- }
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 6.0"
+    }
+  }
 }
 
 provider "google" {
- project = "thinking-pagoda-501907-e7"
- region = "us-central1"
+  project = "thinking-pagoda-501907-e7"
+  region  = "us-central1"
 }
 
-resource "google_storage_bucket" "demo_bucket" {
- name = "thinking-pagoda-501907-e7-demo-bucket"
- location = "US"
- uniform_bucket_level_access = true
+resource "google_storage_bucket" "demo" {
+  name                        = "thinking-pagoda-501907-e7-demo-bucket"
+  location                    = "US"
+  uniform_bucket_level_access = true
 }
-``
