bae7d0177637ccb9cf75b8203aaa19a93deb51e3
pragya2007batra <pragya2007batra@gmail.com>
2026-07-21 21:56:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,3 +10,7 @@ provider "google" {
   project = "thinking-pagoda-501907-e7"
   region  = "us-central1"
 }
+resource "google_storage_bucket" "demo" {
+  name     = "thinking-pagoda-501907-e7-demo-bucket-001"
+  location = "US"
+}
